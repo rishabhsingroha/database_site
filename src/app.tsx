ddc7b583067c4a179e93446e2da652e7c9f67a8b
rishabhsingroha <141716230+rishabhsingroha@users.noreply.github.com>
2025-03-19 22:34:38 +0530

    dem

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import Layout from '../components/Layout';
-import Card from '../components/Card';
+import Layout from './components/Layout';
+import Card from './components/Card';
 import { getLeagueById, getTeamsByLeagueId } from './data';
 import { ArrowLeft } from 'lucide-react';
 
